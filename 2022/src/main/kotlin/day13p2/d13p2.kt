039f9a2bacdf874a822ee32cf38bcba1c5a9af69
Kai Meder <kai.meder@sportalliance.com>
2022-12-13 21:09:08 +0100

    day13 part2

---
+++ 2022/src/main/kotlin/day13p2/d13p2.kt
@@ -0,0 +1,121 @@
+package day13p2
+
+import util.getResourceAsFile
+
+fun main() {
+    val div2 = Packet.Ls(mutableListOf(Packet.Ls(mutableListOf(Packet.Num(2)))))
+    val div6 = Packet.Ls(mutableListOf(Packet.Ls(mutableListOf(Packet.Num(6)))))
+
+    val packets = getResourceAsFile("day13.txt").useLines { lines ->
+        lines.chunked(3).flatMapIndexed { idx, chunk ->
+            val p1 = Parser.parse(chunk[0])
+            val p2 = Parser.parse(chunk[1])
+            listOf(p1, p2)
+        }.toList()
+    } + listOf(div2, div6)
+
+    val sorted = packets.sorted()
+    val div2Pos = sorted.indexOf(div2) + 1
+    val div6Pos = sorted.indexOf(div6) + 1
+    println(div2Pos * div6Pos)
+}
+
+sealed interface Packet : Comparable<Packet> {
+
+    fun isSmallerThan(other: Packet): Boolean = this <= other
+
+    override fun compareTo(other: Packet): Int =
+        when (this) {
+            is Ls -> when (other) {
+                is Ls -> compareLists(this, other)
+                is Num -> this.compareTo(Ls.wrap(other))
+            }
+            is Num -> when (other) {
+                is Ls -> Ls.wrap(this).compareTo(other)
+                is Num -> this.n.compareTo(other.n)
+            }
+        }
+
+    fun compareLists(ls1: Ls, ls2: Ls): Int {
+        if (ls1.c.isEmpty() && ls2.c.isNotEmpty()) {
+            return -1
+        }
+        for (i in 0 until ls1.c.size) {
+            val itm1 = ls1.c.get(i)
+            val itm2 = ls2.c.getOrNull(i)
+
+            if (itm2 == null) {
+//                println("abort on #$i of\n  $ls1 vs.\n  $ls2 because $itm1 vs. null")
+                return 1 // Right side is smaller, so inputs are not in the right order
+            }
+            val cmp = itm1.compareTo(itm2)
+            if (cmp != 0) {
+//                println("abort on #$i of\n  $ls1 vs.\n  $ls2 because $itm1 vs. $itm2 = $cmp")
+                return cmp
+            }
+        }
+        return ls1.c.size.compareTo(ls2.c.size)
+    }
+
+    data class Ls(
+        val c: MutableList<Packet>
+    ) : Packet {
+        companion object {
+            fun empty() = Ls(mutableListOf())
+
+            fun wrap(num: Num): Ls {
+//                println("WRAP $num")
+                val ls = empty()
+                ls.c.add(num)
+                return ls
+            }
+        }
+    }
+
+    @JvmInline
+    value class Num(
+        val n: Int
+    ) : Packet
+}
+
+object Parser {
+    fun parse(s: String): Packet.Ls {
+        val stack = mutableListOf<Packet.Ls>()
+        var num = ""
+        var root: Packet.Ls? = null
+        s.forEach { c ->
+            when (c) {
+                '[' -> {
+                    val newLs = Packet.Ls.empty()
+                    if (root == null) {
+                        root = newLs
+                    } else {
+                        stack.last().c.add(newLs)
+                    }
+                    stack.add(newLs)
+                }
+                ']' -> {
+                    if (num.isNotEmpty()) {
+                        stack.last().c.add(
+                            Packet.Num(num.toInt())
+                        )
+                        num = ""
+                    }
+                    stack.removeLast()
+                }
+                ',' -> {
+                    if (num.isNotEmpty()) {
+                        stack.last().c.add(
+                            Packet.Num(num.toInt())
+                        )
+                        num = ""
+                    }
+                }
+                else -> {
+                    num += c
+                }
+            }
+        }
+        return root!!
+    }
+}
