00de93b2daf65d87333f732fdc37fd6f35da7448
Kai Meder <kai.meder@sportalliance.com>
2023-12-09 15:26:29 +0100

    day09

---
+++ 2023/src/main/kotlin/day09/Main.kt
@@ -0,0 +1,37 @@
+package day09
+
+import util.getResourceAsFile
+
+typealias NodeMap = Map<String, Pair<String, String>>
+
+fun main() {
+	val hist = getResourceAsFile("day09.in").readText().trim().split("\n")
+		.map { it.split(" ").map { it.toInt() } }
+	println(hist)
+
+	val extras = hist.map {
+//		extrapolate(it)
+		extrapolateBackwards(it)
+	}
+	println(extras)
+
+	println(extras.sum())
+}
+
+fun deltas(xs: List<Int>): List<Int> = xs.windowed(2).map { it[1] - it[0] }
+
+fun extrapolate(xs: List<Int>): Int =
+	if (xs.all { it == 0 }) {
+		0
+	} else {
+		xs.last() + extrapolate(
+			deltas(xs)
+		)
+	}
+
+fun extrapolateBackwards(xs: List<Int>): Int =
+	if (xs.all { it == 0 }) {
+		0
+	} else {
+		xs.first() - extrapolateBackwards(deltas(xs))
+	}
