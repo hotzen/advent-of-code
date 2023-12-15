17de54573d7409f1771ce75184bc7ff8c2bc914d
Kai Meder <kai.meder@sportalliance.com>
2023-12-15 20:52:39 +0100

    day14 revisiting part2

--- 2023/src/main/kotlin/day14/Main.kt
+++ 2023/src/main/kotlin/day14/Main.kt
@@ -16,29 +16,66 @@ fun main() {
 	part2(cols)
 }
 
-fun part2(cols: List<String>) {
-	var foo = cols
+fun part2(init: State) {
+	var state = init
 
-	for (cycle in 1..1_000_000_000) {
-		foo = cycle(foo)
-		if (cycle % 10000 == 0) {
-			println(cycle)
-//			println("cycle #$cycle:\n" + transpose(foo).joinToString("\n") + "\n")
-		}
-	}
-	val load = calcLoad(foo)
-	println("load: $load")
+	val cycleLen = findCycleLength(state)
+	val totalCycles = 1_000_000_000
+	val start = (totalCycles - 1) % cycleLen
+	println("cycleLen: $cycleLen, should start from $start")
+
+//	for (cycle in 1..1_000_000_000) {
+//		state = cycle(state)
+//	}
+//	val load = calcLoad(state)
+//	println("load: $load")
 }
 
-fun cycle(foo: List<String>): List<String> =
-	transpose( // reset
+//fun main() {
+//	val cycleLength = findCycleLength { computeResult(it) }
+//	val totalCycles = 1_000_000_000
+//	val result = computeResult((totalCycles - 1) % cycleLength)
+//	println("Result after $totalCycles cycles: $result")
+//}
+
+fun findCycleLength(init: State): Int {
+
+	var tortoise = cycle(init)
+	var hare = cycle(cycle(init).first)
+	while (tortoise.first != hare.first) {
+		tortoise = cycle(tortoise.first)
+		hare = cycle(cycle(hare.first).first)
+	}
+
+	var mu = 0
+	tortoise = cycle(init)
+	while (tortoise.first != hare.first) {
+		tortoise = cycle(tortoise.first)
+		hare = cycle(hare.first)
+		mu++
+	}
+
+	var lambda = 1
+	hare = cycle(tortoise.first)
+	while (tortoise.first != hare.first) {
+		hare = cycle(hare.first)
+		lambda++
+	}
+
+	return lambda
+}
+
+typealias State = List<String>
+
+fun cycle(state: State): Pair<State, Int> {
+	val newState = transpose( // reset
 		tiltDown(  // east
 			transpose(
 				tiltDown(  // south
 					transpose(
 						tiltUp(  // west
 							transpose(
-								tiltUp(foo) // north
+								tiltUp(state) // north
 							)
 						)
 					)
@@ -46,6 +83,8 @@ fun cycle(foo: List<String>): List<String> =
 			)
 		)
 	)
+	return newState to calcLoad(newState)
+}
 
 fun part1(cols: List<String>) {
 	val tiltedCols = tiltUp(cols)
@@ -54,15 +93,13 @@ fun part1(cols: List<String>) {
 	println("load: $load")
 }
 
-private fun calcLoad(tiltedCols: List<String>): Int {
-	val loadedCols = tiltedCols.map { col ->
+private fun calcLoad(state: State): Int =
+	state.sumOf { col ->
 		col.mapIndexed { y, c ->
-			if (c == 'O') tiltedCols[0].length - y
+			if (c == 'O') state[0].length - y
 			else 0
 		}.sum()
 	}
-	return loadedCols.sum()
-}
 
 fun tiltUp(ss: List<String>): List<String> = ss.map { s ->
 	s.split("#").joinToString("#") { tilt(it) }
